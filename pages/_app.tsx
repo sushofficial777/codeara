3d8556f8beb002c2b44fffa99e7082af5e11d27e
unknown <sushilpandit@creedinfotech.com>
2023-10-14 19:03:16 +0700

    24 aug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// IMPORTING COMPONENTS............
+import NavBar from '@/src/Components/NavBar/NavBar'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <NavBar/>
+    <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+   <>
+   <div className=" bg-blue-400">
+    <h1 className='bg-blue-700'>fdjhjdhj</h1>
+   </div>
+   </>
+  )
+}
